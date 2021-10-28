55ddc8783aeb61dbbe8f1d8b9e7832808a98bb5f
Likianta <likianta@foxmail.com>
2021-10-29 01:38:28 +0800

    Create `IdManager`

--- declare_qtquick/control/__init__.py
+++ declare_qtquick/control/__init__.py
@@ -1,4 +1,5 @@
 from .context_manager import ctx_mgr
 from .id_system import gen_id
 from .id_system import id_gen
+from .id_system import id_mgr
 from .prop_getter_and_setter import PropGetterAndSetter
--- declare_qtquick/control/id_system.py
+++ declare_qtquick/control/id_system.py
@@ -5,7 +5,7 @@ from .__external__ import TsContext as T
 
 class IdGenerator:
     
-    def __init__(self, prefix='_'):
+    def __init__(self, prefix='qid_'):
         self.level = 0
         self._id_chain = defaultdict(int)  # type: T.IdChain
         self._prefix = prefix
@@ -18,8 +18,40 @@ class IdGenerator:
         self.level -= 1
     
     def gen_id(self) -> str:
-        return self._prefix + '_'.join(map(hex, self._id_chain))
+        return self._prefix + '_'.join(
+            hex(self._id_chain[k]) for k in sorted(self._id_chain)
+        )
+    
+    @property
+    def root_id(self):
+        return self._prefix + '0x1'
+
+
+class IdManager:
+    _tile_struct: dict
+    _relations: dict
+    
+    def __init__(self):
+        self._tile_struct = {}
+    
+    def set(self, qid: T.Qid, comp: T.Component):
+        self._tile_struct[qid] = comp
+    
+    def finalized(self):
+        self._relations = defaultdict(list)
+        for qid, comp in self._tile_struct.items():
+            # assert '_' in qid
+            parent_qid = qid.rsplit('_', 1)[0]
+            self._relations[parent_qid].append(comp)
+    
+    def get_component(self, qid: T.Qid) -> T.Component:
+        return self._tile_struct[qid]
+    
+    def get_children(self, qid: T.Qid) -> T.QidList:
+        return self._relations.get(qid, [])
 
 
 id_gen = IdGenerator()
 gen_id = id_gen.gen_id
+
+id_mgr = IdManager()
