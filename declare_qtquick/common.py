d67a8e4947bbeb9ed3ff21071dabff65edd15601
Likianta <likianta@foxmail.com>
2021-10-31 17:56:34 +0800

    Create common.py

---
+++ declare_qtquick/common.py
@@ -0,0 +1,21 @@
+import os
+import os.path
+from inspect import currentframe
+
+
+def get_current_dir(_level=1):
+    """
+    This is learnt from lk-logger:
+        lk_logger.sourcemap.FrameFinder
+            getframe
+            getinfo
+    """
+    frame = currentframe()
+    for i in range(_level):
+        frame = frame.f_back
+    filename = frame.f_code.co_filename
+    return os.path.dirname(filename).replace('\\', '/')
+
+
+def current_locate(path_stub):
+    return get_current_dir(2) + '/' + path_stub
