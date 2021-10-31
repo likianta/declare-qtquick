bdbe99274e5d8295cca503a34688fb2ad337c2a3
Likianta <likianta@foxmail.com>
2021-11-01 01:38:40 +0800

    Remove example generated file from git

---
+++ docs/how-does-prop-sheet-work.zh.md
@@ -0,0 +1,5 @@
+# `PropSheet` 是如何工作的
+
+> 关联代码: `declare_qtquick.properties.prop_sheet.PropSheet`.
+
+_TODO_
--- examples/eg01_hello_world.py
+++ examples/eg01_hello_world.py
@@ -9,15 +9,18 @@ with Application() as app:
         win.visible = True
         
         with Rectangle() as rect:
-            rect.anchors.fill = 'parent'
+            rect.anchors.fill = win
             rect.anchors.margins = 10
             rect.color = '#e28086'
             rect.radius = 8
+            
+            with MouseArea() as area:
+                area.anchors.fill = rect
         
         with Text() as txt:
-            txt.anchors.center_in = 'parent'
+            txt.anchors.center_in = win
             txt.text = 'Hello World'
     
-    # app.build('view.qml')
+    app.build('view.qml')
     # app.start('view.qml')
-    app.debug('view.qml')
+    # app.debug('view.qml')
